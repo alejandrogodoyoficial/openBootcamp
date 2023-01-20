7d090d0b856efdc7ab6e4ba745a7ba2db711dc67
Alejandro Godoy <alegodoy2013@gmail.com>
2023-01-20 19:49:12 -0300

    Something

--- cursosDePython/ejercicio12/ejercicio12/asgi.py
+++ cursosDePython/ejercicio12/ejercicio12/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ejercicio12 project.
+ASGI config for tema12 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ejercicio12.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tema12.settings')
 
 application = get_asgi_application()
